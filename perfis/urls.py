d6bc1dd482b5c2306b70f56eb4a072d2a0f70042
alisoncassisn <alisoncassisn@gmail.com>
2017-10-18 23:06:13 -0200

    mais 3 capitulos de django

--- perfis/urls.py
+++ perfis/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^perfis/(?P<perfil_id>[0-9]+)$', views.exibir, name='exibir'),
     url(r'^perfis/(?P<perfil_id>[0-9]+)/convidar$', views.convidar, name='convidar'),
+    url(r'^convite/(?P<convite_id>[0-9]+)/aceitar$', views.aceitar, name='aceitar'),
 ]
